a9f47309eeb276798eec8eb62716bef7587bbf67
al-chaudhari <chaudharia041@gmail.com>
2020-08-28 23:58:42 +0530

    First Working State

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
-const readline = require('readline');
+#!/bin/env node
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+inquirer.registerPrompt(
+  "autocomplete",
+  require("inquirer-autocomplete-prompt")
+);
+let files = fs.readdirSync(path.join(__dirname, "types"));
+let filesLowercase = files
+  .map((el) => el.split(".")[0])
+  .map((e) => e.toLowerCase());
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
-
-rl.question("What is Your Age ? ", (ans) => {
-    console.log(ans);
-    rl.close();
-})
+inquirer
+  .prompt([
+    {
+      type: "autocomplete",
+      name: "file",
+      message: "Select a state to travel from",
+      source: (answersSoFar, input) => {
+        return input
+          ? filesLowercase.filter((e) => e.startsWith(input))
+          : filesLowercase;
+      },
+    },
+  ])
+  .then(function (answers) {
+    let { file } = answers;
+    let one = files.filter((e) => {
+      if (e.split(".")[0].toLowerCase() == file) {
+        return e;
+      }
+    });
+    if (one) {
+      fs.writeFileSync(
+        ".gitignore",
+        fs.readFileSync(path.join(__dirname, "types", one[0]), {
+          encoding: "utf-8",
+        })
+      );
+    }
+  });
